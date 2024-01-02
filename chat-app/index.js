b1df00b669e5b50e1bec34e2c982b7d8eaababd7
nikhilkumar50 <nikhilkumarray9889@gmail.com>
2024-01-02 13:17:01 +0530

    chatApp complete

--- chat-app/index.js
+++ chat-app/index.js
@@ -8,7 +8,10 @@ const server=http.createServer(app);
 const io=new Server(server);
 
 io.on("connection",(socket)=>{
-    console.log("A new user has connected",socket.id);
+    socket.on("user-message",(message)=>{
+        io.emit("message",message);
+
+    })
 })
 
 app.use(express.static(path.resolve("./public")));
